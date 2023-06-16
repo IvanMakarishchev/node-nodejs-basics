a04b4730e92069813278994c4d771abf6865ff57
IvanMakarishchev <denis.flank@gmail.com>
2023-06-16 20:10:56 +0200

    feat: finish list.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,18 +1,22 @@
-import fsProm from 'fs/promises';
+import fsProm from "fs/promises";
 
-const FILE_PATH = './files/';
-const FILE_NAME = 'fileToRemove.txt';
-const FAIL_MESSAGE = 'FS operation failed';
+const FILE_PATH = "./files/";
+const FILE_NAME = "fileToRemove.txt";
+const FAIL_MESSAGE = "FS operation failed";
 
-const isExist = async () => fsProm.stat(FILE_PATH + FILE_NAME).then(() => true).catch(() => false);
+const isExist = async () =>
+  fsProm
+    .stat(FILE_PATH + FILE_NAME)
+    .then(() => true)
+    .catch(() => false);
 
 const remove = async () => {
-    if (!await isExist()) throw new Error(FAIL_MESSAGE);
-    fsProm.rm(FILE_PATH + FILE_NAME);
+  if (!(await isExist())) throw new Error(FAIL_MESSAGE);
+  fsProm.rm(FILE_PATH + FILE_NAME);
 };
 
 try {
-    await remove();
+  await remove();
 } catch (e) {
-    console.error(e);
+  console.error(e);
 }
