844e881fa8af934cf08583f731773b14d970cfb7
IvanMakarishchev <denis.flank@gmail.com>
2023-06-16 14:09:01 +0200

    feat: refactor create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -5,18 +5,13 @@ const FILE_NAME = 'fresh.txt';
 const FILE_PAYLOAD = 'I am fresh and young';
 const FAIL_MESSAGE = 'FS operation failed';
 
-const isExists = async () => fsProm.stat(FILE_PATH + FILE_NAME).then(() => true).catch(() => false);
+const isExists = () => fsProm.stat(FILE_PATH + FILE_NAME).then(() => true).catch(() => false);
 
 const create = async () => {
-    await isExists().then(
-        data => {
-            if (data) {
-                throw new Error(FAIL_MESSAGE)
-            } else {
-                fsProm.writeFile(FILE_PATH + FILE_NAME, FILE_PAYLOAD)
-            }
-        }
-    )
+    if (await isExists()) {
+        throw new Error(FAIL_MESSAGE);
+      }
+    fsProm.writeFile(FILE_PATH + FILE_NAME, FILE_PAYLOAD);
 };
 try {
     await create()
