91d3742d27a71f8b2ca510a659e558b8f309ac09
IvanMakarishchev <denis.flank@gmail.com>
2023-06-16 13:32:45 +0200

    feat: finished create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
-const create = async () => {
-    // Write your code here 
-};
+import fsProm from 'fs/promises';
 
-await create();
+const FILE_PATH = './';
+const FILE_NAME = 'fresh.txt';
+const FILE_PAYLOAD = 'I am fresh and young';
+const FAIL_MESSAGE = 'FS operation failed';
+
+const isExists = async () => fsProm.stat(FILE_PATH + FILE_NAME).then(() => true).catch(() => false);
+
+const create = async () => {
+    await isExists().then(
+        data => {
+            if (data) {
+                throw new Error(FAIL_MESSAGE)
+            } else {
+                fsProm.writeFile(FILE_PATH + FILE_NAME, FILE_PAYLOAD)
+            }
+        }
+    )
+};
+try {
+    await create()
+} catch (e) {
+    console.error(e)
+}
