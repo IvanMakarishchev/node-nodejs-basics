5b3d6d84b0c18c45e21134d4fb3f598c73016927
IvanMakarishchev <denis.flank@gmail.com>
2023-06-16 17:58:08 +0200

    feat: finish delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fsProm from 'fs/promises';
+
+const FILE_PATH = './files/';
+const FILE_NAME = 'fileToRemove.txt';
+const FAIL_MESSAGE = 'FS operation failed';
+
+const isExist = async () => fsProm.stat(FILE_PATH + FILE_NAME).then(() => true).catch(() => false);
+
 const remove = async () => {
-    // Write your code here 
+    if (!await isExist()) throw new Error(FAIL_MESSAGE);
+    fsProm.rm(FILE_PATH + FILE_NAME);
 };
 
-await remove();
+try {
+    await remove();
+} catch (e) {
+    console.error(e);
+}
