2cccd8f597ad71b28800fbc34f3cff1dcd7f35be
IvanMakarishchev <denis.flank@gmail.com>
2023-06-16 20:43:05 +0200

    feat: finish read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,27 @@
+import fsProm from "fs/promises";
+
+const FILE_PATH = "./files/";
+const FILE_NAME = "fileToRead.txt";
+const FAIL_MESSAGE = "FS operation failed";
+
+const isExist = async () =>
+  fsProm
+    .stat(FILE_PATH + FILE_NAME)
+    .then(() => true)
+    .catch(() => false);
+
 const read = async () => {
-    // Write your code here 
+  if (!(await isExist())) throw new Error(FAIL_MESSAGE);
+  fsProm
+    .readFile(FILE_PATH + FILE_NAME, {encoding: 'utf-8'})
+    .then((data) => console.log(data))
+    .catch((err) => {
+      throw new Error(err);
+    });
 };
 
-await read();
+try {
+  await read();
+} catch (e) {
+  console.error(e);
+}
