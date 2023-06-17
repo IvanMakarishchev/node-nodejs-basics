f3b2aa2706b0fa1dfcca5e392112280b5b8bb23e
IvanMakarishchev <denis.flank@gmail.com>
2023-06-17 17:08:32 +0200

    fix: refactor compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,14 +1,14 @@
 import { createReadStream, createWriteStream } from "fs";
-import { deflate, createGzip } from "zlib";
+import { createGzip } from "zlib";
 
 const FILE_PATH = "./files/";
 const SOURCE_FILE = "fileToCompress.txt";
 const DEST_FILE = "archive.gz";
 
 const compress = async () => {
-  const readable = createReadStream(FILE_PATH + SOURCE_FILE);
+  const readable = createReadStream(FILE_PATH + SOURCE_FILE, );
   const writable = createWriteStream(FILE_PATH + DEST_FILE);
   readable.pipe(createGzip()).pipe(writable);
-};
+}
 
 await compress();
