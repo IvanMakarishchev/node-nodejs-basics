52db6b82265ed40c1523a2abe7e55108c394f7b3
IvanMakarishchev <denis.flank@gmail.com>
2023-06-17 11:20:24 +0200

    feat: removed magic string

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,6 +1,9 @@
 import { createReadStream } from "fs";
 
-const readable = createReadStream("./files/fileToRead.txt");
+const PATH_TO_FILE = './files/';
+const FILE_TO_READ = 'fileToRead.txt';
+
+const readable = createReadStream(PATH_TO_FILE + FILE_TO_READ);
 
 const read = async () => {
   readable.pipe(process.stdout);
