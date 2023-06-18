fb87bf6e92b8e2e588f5d19c5dbc028dba08e5ae
IvanMakarishchev <denis.flank@gmail.com>
2023-06-18 16:26:52 +0200

    feat: finished cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,9 +1,12 @@
-import { fork } from 'child_process';
+import { fork } from "child_process";
 
-const CHILD_MODULE = './files/script.js';
+const CHILD_MODULE = "./files/script.js";
+const STDIO_OPTS = ["pipe", "pipe", "ipc"];
 
 const spawnChildProcess = async (args) => {
-    fork(CHILD_MODULE, args);
+  const childProcess = fork(CHILD_MODULE, args, { stdio: STDIO_OPTS });
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
 };
 
 spawnChildProcess([1, 2, 3, 4, 5]);
